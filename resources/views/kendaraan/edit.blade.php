b5f18a36cc0ab1836969d70a1996f94c20fe04d6
irulhaqq <bemybestfriendruli@gmail.com>
2023-12-29 03:09:55 +0700

    Re-design Dashboard
    changes made!

--- resources/views/kendaraan/edit.blade.php
+++ resources/views/kendaraan/edit.blade.php
@@ -1,50 +1,123 @@
-<!-- resources/views/kendaraan/edit.blade.php -->
-
+<!-- resources/views/kendaraans/create.blade.php -->
 @extends('layouts.app')
 
 @section('content')
-    <div class="container mx-auto p-6">
-        <div class="max-w-md mx-auto bg-white rounded-lg overflow-hidden shadow-md">
-            <div class="bg-blue-500 text-white py-2 px-4 font-semibold text-xl text-center">Edit Kendaraan</div>
-            <form method="post" action="{{ route('kendaraan.update', $kendaraans->id) }}" class="px-6 py-4" enctype="multipart/form-data">
-                @csrf
-                @method('PUT')
+<div class="container mx-auto p-6 flex justify-start space-x-4">
+<div class="w-full pr-4">
+    <div class="full-w-md bg-gray rounded-lg overflow-hidden shadow-md">
+    <form method="post" action="{{ route('kendaraan.update', $kendaraans->id) }}" class="px-6 py-4" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
 
+            <div class="grid grid-cols-2 gap-4"> 
+            <div class="col-span-1">
                 <div class="mb-4">
-                    <label for="Tipe_Kendaraan" class="text-gray-600 font-semibold mb-2">Tipe Kendaraan:</label>
-                    <input type="text" class="input input-bordered w-full focus:ring focus:ring-blue-300" id="Tipe_Kendaraan" name="Tipe_Kendaraan" value="{{ old('Tipe_Kendaraan', $kendaraans->Tipe_Kendaraan) }}" required>
+                    <label for="Tipe_Kendaraan" class="block text-gray-600 font-semibold mb-2">Tipe Kendaraan:</label>
+                    <input type="text" class="text-black bg-gray-300 input input-bordered w-full focus:ring focus:ring-blue-500" name="Tipe_Kendaraan" value="{{ old('Tipe_Kendaraan', $kendaraans->Tipe_Kendaraan) }}" required>
                 </div>
-
                 <div class="mb-4">
-                    <label for="Nama_Kendaraan" class="text-gray-600 font-semibold mb-2">Nama Kendaraan:</label>
-                    <input type="text" class="input input-bordered w-full focus:ring focus:ring-blue-300" id="Nama_Kendaraan" name="Nama_Kendaraan" value="{{ old('Nama_Kendaraan', $kendaraans->Nama_Kendaraan) }}" required>
+                    <label for="Nama_Kendaraan" class="block text-gray-600 font-semibold mb-2">Nama Kendaraan:</label>
+                    <input type="text" class="text-black bg-gray-300 input input-bordered w-full focus:ring focus:ring-blue-500" name="Nama_Kendaraan" value="{{ old('Nama_Kendaraan', $kendaraans->Nama_Kendaraan) }}" required>
                 </div>
-
                 <div class="mb-4">
-                    <label for="Harga_Kendaraan" class="text-gray-600 font-semibold mb-2">Harga Kendaraan (Rp):</label>
-                    <input type="number" class="input input-bordered w-full focus:ring focus:ring-blue-300" id="Harga_Kendaraan" name="Harga_Kendaraan" value="{{ old('Harga_Kendaraan', $kendaraans->Harga_Kendaraan) }}" required>
+                    <label for="Harga_Kendaraan" class="block text-gray-600 font-semibold mb-2">Harga Kendaraan (Rp):</label>
+                    <input type="text" class="text-black bg-gray-300 input input-bordered w-full focus:ring focus:ring-blue-500" name="Harga_Kendaraan" value="{{ old('Harga_Kendaraan', $kendaraans->Harga_Kendaraan) }}" required>
                 </div>
-
                 <div class="mb-4">
-                    <label for="Jumlah_Kendaraan" class="text-gray-600 font-semibold mb-2">Jumlah Kendaraan:</label>
-                    <input type="number" class="input input-bordered w-full focus:ring focus:ring-blue-300" id="Jumlah_Kendaraan" name="Jumlah_Kendaraan" value="{{ old('Jumlah_Kendaraan', $kendaraans->Jumlah_Kendaraan) }}" required>
+                    <label for="Jumlah_Kendaraan" class="block text-gray-600 font-semibold mr-2">Jumlah Kendaraan:</label>
+                    <button type="button" onclick="decrement()" class="btn btn-error px-4 py-3 rounded-md focus:outline-none">-</button>
+                    <input type="number" class="text-black bg-gray-300 input input-bordered w-20 text-center focus:ring focus:ring-blue-500" name="Jumlah_Kendaraan" id="Jumlah_Kendaraan" value="{{ old('Jumlah_Kendaraan', $kendaraans->Jumlah_Kendaraan) }}" min="0" onkeydown="return false" onkeyup="return false" required style="-moz-appearance: textfield;">
+                    <button type="button" onclick="increment()" class="btn btn-success px-4 py-3  rounded-md focus:outline-none">+</button>
                 </div>
-
                 <div class="mb-4">
-                    <label for="Deskripsi_Kendaraan" class="text-gray-600 font-semibold mb-2">Deskripsi Kendaraan:</label>
-                    <textarea class="textarea textarea-bordered w-full focus:ring focus:ring-blue-300" id="Deskripsi_Kendaraan" name="Deskripsi_Kendaraan" rows="4" required>{{ old('Deskripsi_Kendaraan', $kendaraans->Deskripsi_Kendaraan) }}</textarea>
+                    <label for="Deskripsi_Kendaraan" class="block text-gray-600 font-semibold mb-2">Deskripsi Kendaraan:</label>
+                    <textarea type="text" class="text-black bg-gray-300 input input-bordered w-full focus:ring focus:ring-blue-500" name="Deskripsi_Kendaraan" rows="4" style="height: 200px;" required>{{ old('Deskripsi_Kendaraan', $kendaraans->Deskripsi_Kendaraan) }}</textarea>
                 </div>
-
-                <div class="mb-4">
-                    <label for="images" class="block text-gray-600 font-semibold mb-2">Images:</label>
-                    <input type="file" class="form-control" name="images[]" accept="image/*" multiple>
-                </div>
-
+            </div>
+            <div class="col-span-1 flex flex-col">
+                <div class="border p-4 rounded-md mb-4">
+                    <div class="mb-4">
+                        <label for="images" class="block text-gray-600 font-semibold mb-2">Upload an Image:</label>
+                        <input type="file" class="form-control mb-2" name="images[]" accept="image/*" multiple onchange="previewImages(event)" />
+                    </div>
+                <div id="imagePreview" class="flex flex-wrap">
+                        <div id="carousel-{{ $kendaraans->id }}" class="carousel w-full">
+                            @if ($kendaraans->images->count() > 0)
+                                @foreach ($kendaraans->images as $index => $image)
+                                <div id ="carousel-{{ $kendaraans->id }}-{{ $index }}" class="carousel-item w-full relative">
+                                        <img src="{{ asset('storage/' . $image->image_path) }}" class="rounded-box w-full h-64 object-cover" alt="Kendaraan Image">
+                                        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                                            <a href="#carousel-{{ $kendaraans->id }}-{{ $index - 1 }}" class="btn btn-circle btn-ghost hover:btn-secondary transition duration-300 ease-in-out transform hover:-translate-y-1">❮</a> 
+                                            <a href="#carousel-{{ $kendaraans->id }}-{{ $index + 1}}" class="btn btn-circle btn-ghost hover:btn-secondary transition duration-300 ease-in-out transform hover:-translate-y-1">❯</a>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            @else
+                                <div class="carousel-item w-full">
+                                    <img src="{{ asset('path/to/default/image.jpg') }}" class="rounded-box w-full h-64 object-cover" alt="Default Image">
+                                </div>
+                            @endif
+                        </div>
+                    </div>
                 <div class="mt-6 flex items-center justify-between">
-                    <a href="{{ route('kendaraan.mykendaraan') }}" class="btn btn-neutral hover:btn-secondary transition duration-300 ease-in-out transform hover:-translate-y-1">Back</a>
-                    <button type="submit" class="btn btn-neutral transition-transform transform hover:scale-105 hover:bg-blue-700">Update Kendaraan</button>
+                    <a href="{{ route('kendaraan.mykendaraan') }}" class="btn btn-error hover:btn-error transition duration-300 ease-in-out transform hover:-translate-y-1">Back</a>
+                    <button type="submit" class="btn btn-success hover:btn-success transition duration-300 ease-in-out transform hover:-translate-y-1">Update Kendaraan</button>
                 </div>
-            </form>
         </div>
     </div>
+</form>
+
+<script>
+    function previewImages(event) {
+        var preview = document.querySelector('#imagePreview');
+        var files = event.target.files;
+
+        preview.innerHTML = ''; // Membersihkan pratinjau sebelumnya
+
+        if (files) {
+            [].forEach.call(files, readAndPreview);
+        }
+
+        function readAndPreview(file) {
+            var reader = new FileReader();
+            reader.onloadend = function () {
+                var img = document.createElement('img');
+                img.src = reader.result;
+                img.style.width = 'auto'; // Ukuran pratinjau gambar
+                img.style.height = 'auto';
+                img.style.marginRight = '10px'; // Margin antara pratinjau gambar
+                preview.appendChild(img);
+            }
+            if (file) {
+                reader.readAsDataURL(file);
+            }
+        }
+    }
+
+    function increment() {
+        const input = document.getElementById('Jumlah_Kendaraan');
+        const value = parseInt(input.value, 10);
+        input.value = value + 1;
+    }
+
+    function decrement() {
+        const input = document.getElementById('Jumlah_Kendaraan');
+        const value = parseInt(input.value, 10);
+        if (value > 0) {
+            input.value = value - 1;
+        }
+    }
+</script>
+
+<style>
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    input[type=number] {
+    -moz-appearance: textfield;
+    }
+</style>
 @endsection
--- resources/views/payment/index.blade.php
+++ resources/views/payment/index.blade.php
@@ -1,34 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container mx-auto p-5">
-        <h1 class="text-3xl mb-4">Payment Details</h1>
+    <div class="container mx-auto p-5 flex justify-center">
         @foreach ($rentals as $rental)
         @endforeach
-        <form action="{{ route('payment.process', ['rentalId' => $rental->id]) }}" method="POST">
+        <form action="{{ route('payment.process', ['rentalId' => $rental->id]) }}" method="POST" class="bg-white border rounded p-5 w-4/12">
             @csrf
-
-            <div class="mb-4">
+            <h1 class="text-3xl text-black mb-4 text-center">Payment Details</h1>
+            <div class="mb-3">
                 <label for="card_number" class="block text-sm font-medium text-gray-700">Card Number</label>
-                <input type="text" name="card_number" id="card_number" class="form-input mt-1 block w-full" required>
+                <input type="text" name="card_number" id="card_number" class="form-input mt-1 block w-full bg-gray-200 border-0 text-black" placeholder="Enter Card Number" required>
             </div>
 
-            <div class="mb-4">
-                <label for="expiration_date" class="block text-sm font-medium text-gray-700">Expiration Date</label>
-                <input type="text" name="expiration_date" id="expiration_date" class="form-input mt-1 block w-full" required>
-            </div>
-
-            <div class="mb-4">
-                <label for="cvv" class="block text-sm font-medium text-gray-700">CVV</label>
-                <input type="text" name="cvv" id="cvv" class="form-input mt-1 block w-full" required>
+            <div class="mb-4 flex">
+                <div class="w-3/5">
+                    <label for="expiration_date" class="block text-sm font-medium text-gray-700">Expiration Date</label>
+                    <input type="text" name="expiration_date" id="expiration_date" class="form-input mt-1 block w-full bg-gray-200 border-0 text-black" placeholder="MM/YYYY" required>
+                </div>
+                <div class="w-2/5 pl-4">
+                    <label for="cvv" class="block text-sm font-medium text-gray-700">CVV</label>
+                    <input type="text" name="cvv" id="cvv" class="form-input mt-1 block w-full bg-gray-200 border-0 text-black" placeholder="CVV" required>
+                </div>
             </div>
 
             <div class="mb-4">
                 <label for="billing_address" class="block text-sm font-medium text-gray-700">Billing Address</label>
-                <textarea name="billing_address" id="billing_address" class="form-textarea mt-1 block w-full" rows="3" required></textarea>
+                <textarea name="billing_address" id="billing_address" class="form-textarea mt-1 block w-full bg-gray-200 border-0 text-black" placeholder="Enter Billing Address" rows="3" required></textarea>
             </div>
 
-            <button type="submit" class="btn btn-primary">Submit Payment</button>
+            <button type="submit" class="btn btn-info hover:btn-info transition duration-300 ease-in-out transform hover:-translate-y-1 w-full">Submit Payment</button>
         </form>
     </div>
 @endsection
